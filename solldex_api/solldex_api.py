8a8649af5f96f5d99200eddef7c0cc88bcaded53
CTO <cto@filterfeed.com.br>
2023-06-15 14:06:11 -0300

    Title: Return JSON Response from Solldex API
    Description: Updated the SolldexAPI class to return the JSON response from the API endpoints. This change allows the caller to have direct access to the API response data, making it easier to handle and process the data as needed.

--- solldex_api/solldex_api.py
+++ solldex_api/solldex_api.py
@@ -23,7 +23,7 @@ class SolldexAPI:
         }
 
     @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
-    def recepcionar_lote(self, data: Dict[str, Union[str, int, Dict]]) -> requests.Response:
+    def recepcionar_lote(self, data: Dict[str, Union[str, int, Dict]]) -> Dict:
         """
         Makes a POST request to the 'recepcionar-lote-rps' endpoint of the Solldex API.
 
@@ -37,13 +37,13 @@ class SolldexAPI:
         try:
             response = requests.post(url, headers=self.headers, json=data)
             response.raise_for_status()
-            return response
+            return response.json()
         except requests.RequestException as e:
             logging.error(f'Request to {url} failed: {e}')
             raise
 
     @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
-    def consultar_lote(self, data: Dict[str, Union[str, int]]) -> requests.Response:
+    def consultar_lote(self, data: Dict[str, Union[str, int]]) -> Dict:
         """
         Makes a GET request to the 'consulta-lote' endpoint of the Solldex API.
 
@@ -57,13 +57,13 @@ class SolldexAPI:
         try:
             response = requests.get(url, headers=self.headers, json=data)
             response.raise_for_status()
-            return response
+            return response.json()
         except requests.RequestException as e:
             logging.error(f'Request to {url} failed: {e}')
             raise
 
     @retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
-    def consultar_rps(self, data: Dict[str, Union[str, int]]) -> requests.Response:
+    def consultar_rps(self, data: Dict[str, Union[str, int]]) -> Dict:
         """
         Makes a GET request to the 'consulta-rps' endpoint of the Solldex API.
 
@@ -77,7 +77,7 @@ class SolldexAPI:
         try:
             response = requests.get(url, headers=self.headers, json=data)
             response.raise_for_status()
-            return response
+            return response.json()
         except requests.RequestException as e:
             logging.error(f'Request to {url} failed: {e}')
             raise
